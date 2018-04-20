cf74ad44ff356f35c708a2dc4b2c81ecc53ec4f5
side105 <side105@hotmail.com>
2018-04-20 23:53:45 +0200

    deleted exlamation mark to posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', (req, res)=>{
-    res.json({msg: "Posts Works!"});
+    res.json({msg: "Posts Works"});
 });
 //ciao
 module.exports =  router;
